e0276b8d0e4d79bdf2368e71132f63926fa77813
Carl <crlgndrsn@gmail.com>
2016-04-30 16:32:27 -0700

    Add front-end build
    Includes sass, webpack, and npm build tasks

---
+++ js/index.js
@@ -0,0 +1,3 @@
+var title = document.createElement('h1')
+title.innerHTML = 'Carl Gunderson'
+document.querySelector('.container-fluid').appendChild(title)
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	entry: './js/index.js',
+	output: {
+		path: __dirname + '/dist',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: []
+	}
+};
