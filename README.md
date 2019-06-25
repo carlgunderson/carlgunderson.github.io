5139c69987512bc3785544ca2f2476ea2a1abce1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-25 19:29:44 +0000

    Deploy carlgunderson/carlgunderson.github.io to github.com/carlgunderson/carlgunderson.github.io.git:gh-pages

--- README.md
+++
@@ -1,5 +0,0 @@
-## Web development portfolio of Carl Gunderson
-
-Developed with **React**, **Material UI**, and **ES6/ES2015**.
-
-Built with **webpack**.
--- webpack.prod.babel.js
+++
@@ -1,7 +0,0 @@
-import merge from 'webpack-merge'
-import common from './webpack.common.babel.js'
-
-export default merge(common, {
-	mode: 'production',
-	devtool: 'source-map',
-})
