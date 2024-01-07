5900c1a27cee86cb57db7d3fbcf7be44bd4eeadd
Carl Gunderson <crlgndrsn@gmail.com>
2024-01-07 15:41:42 -0800

    mobile scroll up

--- src/components/JobItem/index.jsx
+++ src/components/JobItem/index.jsx
@@ -25,106 +25,94 @@ const JobItem = ({ idx, isActive, item, onClick, onNav }) => {
 	}
 
 	return (
-		<Box
-			key={ item.slug }
-			sx={{
-				width: ['100%', 'auto'],
-				display: 'flex',
-				flexDirection: 'column',
-				justifyContent: 'center',
-				alignItems: 'center',
-				mb: '60px',
-			}}
-		>
-			<Fade in={ isActive }>
+		<Fade in={ isActive }>
+			<Box
+				sx={{
+					width: '100%',
+					height: ['calc(100vh - 276px)', 'calc(100vh - 164px)'],
+					position: 'absolute',
+					top: 0,
+					right: 0,
+					bottom: 0,
+					left: 0,
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+					justifyContent: 'center',
+					bgcolor: isFilled ? item.bgColor : '#fff',
+					color: isFilled ? '#fff' : item.bgColor,
+					overflow: 'hidden',
+				}}
+			>
+				<Arrow
+					direction='prev'
+					item={ item }
+					onClick={ handleNav }
+					isDisabled={ idx === 0 }
+					isFilled={ isFilled }
+				/>
 				<Box
+					tabIndex={ 0 }
+					onClick={ e => handleClick(e) }
+					onKeyDown={ e => e.key === 'Enter' && handleClick(e) }
 					sx={{
-						width: '100%',
-						height: ['calc(100vh - 276px)', 'calc(100vh - 164px)'],
-						position: 'absolute',
-						top: 0,
-						right: 0,
-						bottom: 0,
-						left: 0,
 						display: 'flex',
 						flexDirection: 'column',
 						alignItems: 'center',
 						justifyContent: 'center',
-						bgcolor: isFilled ? item.bgColor : '#fff',
-						color: isFilled ? '#fff' : item.bgColor,
-						overflow: 'hidden',
+						p: ['60px', '100px'],
+						cursor: 'pointer',
 					}}
 				>
-					<Arrow
-						direction='prev'
-						item={ item }
-						onClick={ handleNav }
-						isDisabled={ idx === 0 }
-						isFilled={ isFilled }
+					<div style={{ width: '100px' }}>
+						<Box
+							ref={ tileRef }
+							className='tile'
+							sx={{
+								width: ['48px', '100px'],
+								height: ['48px', '100px'],
+								m: '0 auto',
+								backgroundImage: `url(${item.logoUrl})`,
+								'&:after': {
+									content: '""',
+									display: 'block',
+									marginTop: '100%',
+								}
+							}}
+						/>
+					</div>
+					<Typography
+						className='tile-display-name'
+						variant='h2'
+						align='center'
+						fontWeight={ 600 }
+						sx={{ color: isFilled ? '#fff' : item.bgColor }}
+						children={ item.displayName }
 					/>
-					<Box
-						tabIndex={ 0 }
-						onClick={ e => handleClick(e) }
-						onKeyDown={ e => e.key === 'Enter' && handleClick(e) }
-						sx={{
-							display: 'flex',
-							flexDirection: 'column',
-							alignItems: 'center',
-							justifyContent: 'center',
-							p: ['60px', '100px'],
-							cursor: 'pointer',
-						}}
-					>
-						<div style={{ width: '100px' }}>
-							<Box
-								ref={ tileRef }
-								className='tile'
-								sx={{
-									width: ['48px', '100px'],
-									height: ['48px', '100px'],
-									m: '0 auto',
-									backgroundImage: `url(${item.logoUrl})`,
-									'&:after': {
-										content: '""',
-										display: 'block',
-										marginTop: '100%',
-									}
-								}}
-							/>
-						</div>
-						<Typography
-							className='tile-display-name'
-							variant='h2'
-							align='center'
-							fontWeight={ 600 }
-							sx={{ color: isFilled ? '#fff' : item.bgColor }}
-							children={ item.displayName }
-						/>
-						<Typography
-							className='tile-role'
-							variant='h5'
-							align='center'
-							fontWeight={ 600 }
-							gutterBottom
-							sx={{ color: isFilled ? '#fff' : item.bgColor }}
-							children={ item.role }
-						/>
-						<Typography
-							align='center'
-							sx={{ color: isFilled ? '#fff' : item.bgColor }}
-							children={ item.timeline }
-						/>
-					</Box>
-					<Arrow
-						direction='next'
-						isDisabled={ idx === jobs.length - 1 }
-						isFilled={ isFilled }
-						item={ item }
-						onClick={ handleNav }
+					<Typography
+						className='tile-role'
+						variant='h5'
+						align='center'
+						fontWeight={ 600 }
+						gutterBottom
+						sx={{ color: isFilled ? '#fff' : item.bgColor }}
+						children={ item.role }
+					/>
+					<Typography
+						align='center'
+						sx={{ color: isFilled ? '#fff' : item.bgColor }}
+						children={ item.timeline }
 					/>
 				</Box>
-			</Fade>
-		</Box>
+				<Arrow
+					direction='next'
+					isDisabled={ idx === jobs.length - 1 }
+					isFilled={ isFilled }
+					item={ item }
+					onClick={ handleNav }
+				/>
+			</Box>
+		</Fade>
 	)
 }
 
